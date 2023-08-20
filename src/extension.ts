0b76195ccbf09e2ff9ea00ec23df5fd93245c513
Sasha Aleshchenko <45843308+TheBigSasha@users.noreply.github.com>
2023-08-20 03:24:34 -0400

    add png to ico support

--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,7 @@ import * as vscode from 'vscode';
 import * as fs from 'fs';
 import * as path from 'path';
 import sharp = require('sharp');
+import ico = require('sharp-ico');
 
 class SVGCodeActionProvider implements vscode.CodeActionProvider {
     public provideCodeActions(document: vscode.TextDocument, range: vscode.Range | vscode.Selection, context: vscode.CodeActionContext, token: vscode.CancellationToken): vscode.ProviderResult<vscode.CodeAction[]> {
@@ -29,6 +30,24 @@ export function activate(context: vscode.ExtensionContext) {
     context.subscriptions.push(
         vscode.languages.registerCodeActionsProvider('svg', new SVGCodeActionProvider())
     );
+    
+    // Add another context item to the extension to generate an ICO
+    context.subscriptions.push(
+        vscode.commands.registerCommand('extension.convertPNGToICO', async (uri: vscode.Uri) => {
+            // inform the user that the image conversion is in progress
+            vscode.window.showInformationMessage('Converting PNG to ICO...');
+            const pngData = fs.readFileSync(uri.fsPath);
+            const icoData = await ico.sharpsToIco([sharp(pngData)], null);
+            const icoPath = path.join(path.dirname(uri.fsPath), path.basename(uri.fsPath, '.png') + '.ico');
+
+            fs.writeFileSync(icoPath, icoData);
+
+            vscode.window.showInformationMessage(`Converted PNG to ICO: ${icoPath}`);
+        })
+    );
 }
 
 
+
+
+//TODO: Ico files https://github.com/lovell/sharp/issues/1118#issuecomment-1205599759
